65429436412546cdf64e613de264b5e1f6b0f998
iSimplyDesign <60794490+DiscardedDev@users.noreply.github.com>
2024-09-30 15:35:38 +0100

    finished bonus challenge

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,26 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    bool canSpawnDog = true;
+
+    [SerializeField] float spawnInterval;
+
+    float currentSpawnInterval = 0;
+    private void Start() {
+        bool canSpawnDog = true;
+    }
     // Update is called once per frame
     void Update()
-    {
+    {   
+        currentSpawnInterval -= Time.deltaTime;
+        if(currentSpawnInterval <= 0){
+            canSpawnDog = true;
+        }
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog) {
+            canSpawnDog = false;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            currentSpawnInterval = spawnInterval;  
         }
     }
 }
